12131ba1d280aa3a3005bd351d19dbbad78c79b0
RDGames <RDGames1000@email.cz>
2022-03-21 18:13:33 +0100

    first commit

---
+++ src/main.c
@@ -0,0 +1,53 @@
+#include "stm8_hd44780.h"
+#include "stm8s.h"
+#include "milis.h"
+
+//#include "delay.h"
+//#include <stdio.h>
+//#include "uart1.h"
+
+#define _ISOC99_SOURCE
+#define _GNU_SOURCE
+
+#define LED_PORT GPIOD
+#define LED_PIN  GPIO_PIN_4
+#define LED_HIGH   GPIO_WriteHigh(LED_PORT, LED_PIN)
+#define LED_LOW  GPIO_WriteLow(LED_PORT, LED_PIN)
+#define LED_REVERSE GPIO_WriteReverse(LED_PORT, LED_PIN)
+
+
+
+void setup(void)
+{
+    CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
+    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
+
+    lcd_init();
+    init_milis();
+    //init_uart1();
+}
+
+
+int main(void)
+{
+    uint32_t time = 0;
+
+    setup();
+    lcd_gotoxy(0,0);
+    lcd_puts("go fuck ur self");
+    while (1) {
+
+        if (milis() - time > 333 ) {
+            LED_REVERSE; 
+            time = milis();
+            //printf("%ld\n", time);
+        }
+
+        //LED_REVERSE; 
+        //delay_ms(333);
+        //printf("Funguje to!!!\n");
+    }
+}
+
+/*-------------------------------  Assert -----------------------------------*/
+#include "__assert__.h"
